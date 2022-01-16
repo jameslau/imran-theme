018077706bde9bab4ff44072a5c5f932f63f5cf1
James Lau <1402829+jameslau@users.noreply.github.com>
2022-01-15 23:16:47 -0500

    video tutorial 28

--- app/public/wp-content/themes/imran/index.php
+++ app/public/wp-content/themes/imran/index.php
@@ -1,6 +1,5 @@
 <?php
 /**
- * 
  * Main template file.
  * 
  * @package Imran
@@ -8,10 +7,31 @@
 
 
 get_header();
- ?>
 
-<div class="content">Content</div>
+?>
 
-<?
+<div id="primary">
+  <main id="main" class="site-main mt-5" role="main">
+    <?php
+      //note: the famous WP loop
+      if ( have_posts() ) {
+        ?>
+        <div class="container">
+          <?php
+            while ( have_posts() ) : the_post();
+              the_title();
+              //the_excerpt();
+              the_content();
+            endwhile;
+          ?>
+        </div>
+        <?php
+      } 
+    ?>
+  </main>
+</div>
+ 
+<?php 
+
 get_footer();
 
---
+++ app/public/wp-content/themes/imran/page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Page template
+ * 
+ * @package Imran
+ */
+
+get_header(); 
+?>
+
+<div>Single Page</div>
+
+<?php get_footer(); ?>
+
