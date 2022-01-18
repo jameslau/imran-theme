54e8c53cae817f04d3a80e7402e757cac10c3313
James Lau <1402829+jameslau@users.noreply.github.com>
2022-01-18 13:23:54 -0500

    video tutorial 30

--- app/public/wp-content/themes/imran/index.php
+++ app/public/wp-content/themes/imran/index.php
@@ -30,13 +30,49 @@ get_header();
               <?php
             }
           ?>
-          <?php
-            while ( have_posts() ) : the_post();
-              the_title();
-              //the_excerpt();
-              the_content();
-            endwhile;
-          ?>
+          <div class="row">
+            <?php
+              
+              $index = 0;
+              $no_of_columns = 1;
+
+              //note: First case when index is 0
+              //note: Second case when index is 1
+              //note: continues cases until it loops through all posts
+              //note: start the loop
+              while ( have_posts() ) : the_post();
+                
+                //note: is first case zero? yes.
+                //note: is second case one? yes.
+                //note: number of index divided by columns is equal to zero. opening tag will show as long as index is zero
+                if ( 0 === $index % $no_of_columns ) {
+                  ?>
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                  <?php
+                }
+                
+                //note: write the content of the HTML here
+                ?>
+                <h3><?php the_title(); ?></h3>
+                <div><?php the_excerpt(); ?></div>
+                <?php                
+
+                //note: index is increment by one (this depends on how many posts you have total)
+                //note: first case, indexes value is equal to one
+                //note: second case, indexes value is equal to two
+                $index++;
+
+                //note: if the index is NOT zero (!==) and if the remainder of index, complete the closing tag
+                if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+                  ?>
+                    </div>
+                  <?php
+                }
+
+
+              endwhile;
+            ?>
+          </div>
         </div>
         <?php
       } 
